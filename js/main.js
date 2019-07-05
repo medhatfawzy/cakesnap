216ab91db3b430afff7e6aa430d51f9909cdce08
iMedhat58 <medoelking58@gmail.com>
2019-07-05 05:22:37 +0200

    update

--- js/main.js
+++ js/main.js
@@ -4,24 +4,42 @@ var passRegex = /^.{5,16}$/;
 var emailRegex = /[a-z]\w{3,7}@\w{3,10}\.\w{2,6}$/i;
 var nameRegx = /^[a-z]{2,12}$/i;
 
-var valdation = ()=>{
-    
+var displayX = (field)=>{
+    let icon = document.getElementById(`${field.id}Icon`);
+    icon.innerHTML = `<i class="fas fa-times"></i>`;
+};
+var removeX = (field)=>{
+    let icon = document.getElementById(`${field.id}Icon`);
+    if(field.id === "name"){
+        icon.innerHTML = `<i class="far fa-user-circle"></i>`;    
+    }
+    else if(field.id === "email"){
+        icon.innerHTML = `<i class="fas fa-envelope">`;
+    }
+    else if(field.id === "password"){
+        icon.innerHTML = `<i class="fas fa-lock">`;
+    }
+
+};
+var valdation = ()=> { 
     let password = document.getElementById("password");
     let email = document.getElementById("email");
     let name = document.getElementById("name");
-
+    let noError = true;
     if(!emailRegex.test(email.value)) {
-        console.log("invalid email");
-    }
-    else if(!passRegex.test(password.value)) {
-        console.log("invalid password");
-    }
-    else if(!nameRegx.test(name.value)) {
-        console.log("invalid name");
-    }
-    else{
-        document.getElementById("userForm").submit();
-    }
+        displayX(email);
+        noError = false;
+    } else removeX(email);
+    if(!passRegex.test(password.value)) {
+        displayX(password);
+        noError = false;
+    } else removeX(password);
+    if(!nameRegx.test(name.value)) {
+        displayX(name);
+        noError = false;
+    } else removeX(name);
+
+    if(noError) document.getElementById("userForm").submit();
 };
 
 submitBut.onclick = valdation;
